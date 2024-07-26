77226aa9817d9a513be17cf0b004f58907954709
Ruyu Yan <ry233@cornell.edu>
2024-07-26 19:22:09 -0400

    updated layout for phone

--- src/components/cards/PublicationCell.tsx
+++ src/components/cards/PublicationCell.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Author {
   name: string;
@@ -32,62 +32,89 @@ interface PublicationCellProps {
   data: PublicationData;
 }
 
-const PublicationCellFigure: React.FC<PublicationCellFigureProps> = ({ data, link }) => (
-  <div className="figure-container">
-    <a href={link}>
-      {data.isImage ? (
-        <>
-          <img src={`${process.env.PUBLIC_URL}${data.src1}`} className="main-image" />
-          {data.src2 && <img src={`${process.env.PUBLIC_URL}${data.src2}`} className="hover-image" />}
-        </>
-      ) : (
-        <video src={`${process.env.PUBLIC_URL}${data.src1}`} autoPlay loop muted playsInline />
-      )}
-    </a>
-  </div>
-);
+// const PublicationCellFigure: React.FC<PublicationCellFigureProps> = ({ data, link }) => (
+//   <div className="figure-container">
+//     <a href={link}>
+//       {data.isImage ? (
+//         <div className="image-container">
+//           <img src={`${process.env.PUBLIC_URL}${data.src1}`} className="main-image" />
+//           <img src={`${process.env.PUBLIC_URL}${data.src2 ? data.src2 : data.src1}`} className="hover-image" />
+//         </div>
+//       ) : (
+//         <video src={`${process.env.PUBLIC_URL}${data.src1}`} autoPlay loop muted playsInline />
+//       )}
+//     </a>
+//   </div>
+// );
+
+const PublicationCellFigure: React.FC<PublicationCellFigureProps> = ({ data, link }) => {
+  const [currentSrc, setCurrentSrc] = useState(data.src1);
+  
+
+  const handleMouseEnter = () => {
+    if (data.src2) {
+      setCurrentSrc(data.src1);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setCurrentSrc(data.src1);
+  };
+
+  return (
+    <div className="figure-container">
+      <a href={link}>
+        {data.isImage ? (
+          <img 
+            src={`${process.env.PUBLIC_URL}${currentSrc}`} 
+            onMouseEnter={handleMouseEnter} 
+            onMouseLeave={handleMouseLeave} 
+          />
+        ) : (
+          <video src={`${process.env.PUBLIC_URL}${data.src1}`} autoPlay loop muted playsInline />
+        )}
+      </a>
+    </div>
+  );
+};
 
 const PublicationCell: React.FC<PublicationCellProps> = ({ data }) => (
   <div className="publication-cell-container">
-    <div style={{display: "flex", gap: "20px", alignItems: "center"}}>
-      <div style={{width: "30%", aspectRatio: "1", display: "flex", alignItems: "center"}}>
-        <PublicationCellFigure data={ data.figure } link={ data.projectLink }/>
-      </div>
-      
-      <div className="card-text">
-        <header>
-          <h3><a href={data.projectLink}>{data.title}</a></h3>
-          <p>
-            {data.authors.map((author, index) => (
-              <React.Fragment key={index}>
-                {author.link ? (
-                  <a
-                    href={author.link}
-                    style={{ fontWeight: author.name === "Ruyu Yan" ? 'bold' : 'normal' }}
-                  >
-                    {author.name}
-                  </a>
-                ) : (
-                  <span style={{ fontWeight: author.name === "Ruyu Yan" ? 'bold' : 'normal' }}>
-                    {author.name}
-                  </span>
-                )}
-                {index < data.authors.length - 1 && ', '}
-              </React.Fragment>
-            ))}
-            <p style={{fontSize: "0.75rem", fontStyle: "italic"}}>{data.conference}, {data.year}</p>
-            <p style={{fontSize: "0.75rem"}}>
-              {data.projectLink && <a className="website-link" href={data.projectLink}>project website</a>}
-              {data.paperLink && data.projectLink && ' / '}
-              {data.paperLink && <a className="website-link" href={data.paperLink}>paper</a>}
-              {data.paperLink && data.codeLink && ' / '}
-              {data.codeLink && <a className="website-link" href={data.codeLink}>code</a>}
-            </p>
+    <PublicationCellFigure data={ data.figure } link={ data.projectLink }/>
+    
+    <div className="card-text">
+      <header>
+        <h3><a href={data.projectLink}>{data.title}</a></h3>
+        <p>
+          {data.authors.map((author, index) => (
+            <React.Fragment key={index}>
+              {author.link ? (
+                <a
+                  href={author.link}
+                  style={{ fontWeight: author.name === "Ruyu Yan" ? 'bold' : 'normal' }}
+                >
+                  {author.name}
+                </a>
+              ) : (
+                <span style={{ fontWeight: author.name === "Ruyu Yan" ? 'bold' : 'normal' }}>
+                  {author.name}
+                </span>
+              )}
+              {index < data.authors.length - 1 && ', '}
+            </React.Fragment>
+          ))}
+          <p style={{fontSize: "0.75rem", fontStyle: "italic"}}>{data.conference}, {data.year}</p>
+          <p style={{fontSize: "0.75rem"}}>
+            {data.projectLink && <a className="website-link" href={data.projectLink}>project website</a>}
+            {data.paperLink && data.projectLink && ' / '}
+            {data.paperLink && <a className="website-link" href={data.paperLink}>paper</a>}
+            {data.paperLink && data.codeLink && ' / '}
+            {data.codeLink && <a className="website-link" href={data.codeLink}>code</a>}
           </p>
-        </header>
-        <div className="description">
-          <p>{data.desc}</p>
-        </div>
+        </p>
+      </header>
+      <div className="description">
+        <p>{data.desc}</p>
       </div>
     </div>
   </div>
